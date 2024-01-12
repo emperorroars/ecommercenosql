a0678974ae2e867fd2bfe0c2ff2c50ae3b04b7d8
emperorroars <daemosares@gmail.com.com>
2024-01-11 22:17:50 -0800

    first

---
+++ models/product.js
@@ -0,0 +1,49 @@
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
+class Product {
+  constructor(title, price, description, imageUrl, id, userId) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
+  }
+  save()
+  {
+    const db=getDb()
+    return db.collection('products')
+    .insertOne(this)
+    .then(result=>{
+     // console.log(result)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+}
+
+/*const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});*/
+
+module.exports = Product;
